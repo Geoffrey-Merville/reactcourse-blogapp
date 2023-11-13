38c98083602cc2b86da615bb1198c81a71ed1063
gmerville-jehann <gmerville@jehann.fr>
2023-11-13 15:36:59 +0100

    created first useState logic

--- full-react-course/src/components/Content.js
+++ full-react-course/src/components/Content.js
@@ -1,30 +1,29 @@
-import React from "react";
+import { useState } from "react";
 
 const Content = () => {
+  const [name, setName] = useState("");
+  const [count, setCount] = useState(0);
   const handleNameChange = () => {
-    const names = ["Bob", "Kevin", "Dave"];
-    const int = Math.floor(Math.random() * 3);
-    return names[int];
+    const names = ["Bob", "Kevin", "Dave", "Kuragi"];
+    const int = Math.floor(Math.random() * 4);
+    setName(names[int]);
   };
 
   const handleClick = () => {
-    console.log("Clicked");
+    setCount(count + 1);
+    console.log(count);
   };
 
-  const handleClick2 = (name) => {
-    console.log(`${name} Clicked`);
-  };
-
-  const handleClick3 = (e) => {
-    console.log(e.target.innerText);
+  const handleClick2 = () => {
+    console.log(count);
   };
 
   return (
     <main>
-      <p onDoubleClick={handleClick}>Hello {handleNameChange()}!</p>
-      <button onClick={handleClick}>Click it</button>
-      <button onClick={() => handleClick2("Kuragi")}>Click it</button>
-      <button onClick={(e) => handleClick3(e)}>Click it</button>
+      <p onDoubleClick={handleClick}>Hello {name}!</p>
+      <button onClick={handleNameChange}>Change name</button>
+      <button onClick={handleClick}>Increase Counter</button>
+      <button onClick={handleClick2}>Click it</button>
     </main>
   );
 };
