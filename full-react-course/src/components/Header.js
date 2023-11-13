e4fc2a247fddf0ccf1b1f73cfcd8daf4972d1843
gmerville-jehann <gmerville@jehann.fr>
2023-11-13 15:08:56 +0100

    finished creating components + cleaned files and code

--- full-react-course/src/components/Header.js
+++ full-react-course/src/components/Header.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 const Header = () => {
+  /*const headerStyle = {
+    backgroundColor: "mediumblue",
+    color: "#fff",
+  };*/
   return (
     <header>
       <h1>Groceries List</h1>
