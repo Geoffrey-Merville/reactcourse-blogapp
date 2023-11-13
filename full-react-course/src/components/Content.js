e5530f335642fcdd3b43c79882a269aea34253bf
gmerville-jehann <gmerville@jehann.fr>
2023-11-13 15:25:49 +0100

    created first events

--- full-react-course/src/components/Content.js
+++ full-react-course/src/components/Content.js
@@ -7,9 +7,24 @@ const Content = () => {
     return names[int];
   };
 
+  const handleClick = () => {
+    console.log("Clicked");
+  };
+
+  const handleClick2 = (name) => {
+    console.log(`${name} Clicked`);
+  };
+
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  };
+
   return (
     <main>
-      <p>Hello {handleNameChange()}!</p>
+      <p onDoubleClick={handleClick}>Hello {handleNameChange()}!</p>
+      <button onClick={handleClick}>Click it</button>
+      <button onClick={() => handleClick2("Kuragi")}>Click it</button>
+      <button onClick={(e) => handleClick3(e)}>Click it</button>
     </main>
   );
 };
